4b8f26e87655b1e71bdf13eb4bcf69a898b6401f
UncleKhab <cobrosoffice@gmail.com>
2020-11-25 23:51:37 +0200

    06 - deployment failed

--- project3/settings.py
+++ project3/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+')
+
+SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-export DJANGO_DEBUG=False
+DEBUG = True
+
 ALLOWED_HOSTS = []
 
 
