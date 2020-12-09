0e1c5b23b4755d00661016bc8f5ef546084603ad
UncleKhab <cobrosoffice@gmail.com>
2020-12-09 22:45:32 +0200

    e

--- mail/views.py
+++ mail/views.py
@@ -30,7 +30,7 @@ def compose(request):
         return JsonResponse({"error": "POST request required."}, status=400)
 
     # Check recipient emails
-    data = json.loads(request.body)
+    data = json.loads(request.data)
     emails = [email.strip() for email in data.get("recipients").split(",")]
     if emails == [""]:
         return JsonResponse({
